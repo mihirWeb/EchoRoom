3526ac3534051a7fb231bbf1655d2be2a754f1df
mihirWeb <mihirpratapsingh166@gmail.com>
2024-12-31 17:31:11 +0530

    initialize React + TypeScript + Vite project with basic configuration and structure

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -2,17 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-console.log(wss);
+// console.log(wss);
 let userCount = 0;
+let allSockets = [];
 // When a user connects to the server
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     userCount++;
     console.log(`User connected. Total users: ${userCount}`);
     // When a user sends a message
     socket.on("message", (message) => {
         console.log(`Received message: ${message}`); // up untill here one way communication is done
-        setTimeout(() => {
-            socket.send(`You sent: ${message}`); // used to send message back to the client(the one who sent the message)
-        }, 1000);
+        // Send the message to all users
+        allSockets.forEach((s) => {
+            if (s !== socket) {
+                s.send(`sent the message: ${message}`);
+            }
+        });
     });
 });
